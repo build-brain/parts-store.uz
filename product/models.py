36e779ebba09eaa8ea72234296f9e62e785140ba
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2022-06-11 11:31:24 +0500

    Migration

--- product/models.py
+++ product/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-# class Product(models.Model):
-#     title = models.CharField(max_length=255)
-#     description = models.CharField()
-#     # characteristic = 
-#     time_create = models.DateTimeField(auto_now_add=True)
-#     time_update = models.DateTimeField(auto_now=True)
-#     amount = models.BooleanField()
-#     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    # characteristic = 
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    amount = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
-#     def __str__(self):
-#         return self.title 
+    def __str__(self):
+        return self.title 
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=100, db_index=True)
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
